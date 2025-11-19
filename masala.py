85ae4ddfa04525c8259dd1678503a7c220121bd2
Qurbonboyev Salimbek <matniyozovasharofat9@gmail.com>
2025-11-19 16:33:37 +0300

    Add files via upload

---
+++ masala.py
@@ -0,0 +1,51 @@
+# # 1
+#
+#
+#
+# print("1 dan 10 gacha sonlar:")
+#
+# for i in range(1, 11):
+#     print(i)
+
+# # 2
+# ismlar = ["Ali", "Vali", "Hasan", "Malika", "Zuhra"]
+#
+# for ism in ismlar:
+#     print(f"Salom {ism}!")
+
+# # 3
+# print("1 dan 100 gacha bolgan sonlar:")
+# for son in range(0,101,2):
+#     print(son)
+
+# # 4
+# son = 0
+# for son1 in range(1,51):
+#     son = son + son1
+#     print(son)
+
+# # 5
+# matn = input("Matn kiriting:")
+# uznlikk = 0
+# for belgi in matn:
+#     uznlikk = uznlikk + 1
+# print("Matn uzunligi:",uznlikk, "belgi")
+
+# # 6
+# print("/n1 dan 50 gacha 3 ga bolinadigan sonlar")
+# ozgaruvchi = 0
+# for son in range(1, 50):
+#     if son % 3 == 0:
+#         print(son)
+#         ozgaruvchi = ozgaruvchi + 1
+#         print("Jami son:", ozgaruvchi , "son")
+
+# # 7
+# matn = input("\n Matn kiriting: ")
+# unlilar = "asdfghjkllqwwerty"
+# sanoq = 0
+# for belgi in matn.lower():
+#     if belgi in unlilar:
+#         sanoq = sanoq + 1
+# print("Jami unli harflar:", sanoq, "ta")
+
