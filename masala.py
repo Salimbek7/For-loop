e245fc70d25627602eb97dad5c08a002bd60b458
Qurbonboyev Salimbek <matniyozovasharofat9@gmail.com>
2025-11-21 16:13:31 +0300

    Add files via upload

--- masala.py
+++ masala.py
@@ -49,3 +49,61 @@
 #         sanoq = sanoq + 1
 # print("Jami unli harflar:", sanoq, "ta")
 
+# # 8
+# sonlar = [23, 67, 12, 89, 45, 34, 91, 56, 78]
+#
+# eng_katta = sonlar[0]
+#
+# for son in sonlar:
+#     if son > eng_katta:
+#         eng_katta = son
+#
+# print("Eng katta son:", eng_katta)
+
+# # 9
+#
+# son = input("Son kiriting: ")
+#
+#
+# yigindi = 0
+#
+# for belgi in son:
+#     if belgi.isdigit():
+#         yigindi += int(belgi)
+#
+# print(f"{son} sonining raqamlari yig'indisi: {yigindi}")
+
+# # 10
+#
+# son = int(input("Son kiriting: "))
+#
+# if son < 0:
+#     print("Manfiy sonlar uchun faktorial aniqlanmagan!")
+#
+# elif son == 0 or son == 1:
+#     print(f"{son}! = 1")
+#
+# else:
+#     faktorial = 1
+#     for i in range(1, son + 1):
+#         faktorial *= i
+#     print(f"{son}! = {faktorial}")
+
+# # 11
+# print("Multiplikatsiya jadvali (1–10)\n")
+#
+# print("   ", end="")
+# for i in range(1, 11):
+#     print(f"{i:4}", end="")
+# print()
+#
+# print("   " + "-" * 40)
+# for i in range(1, 11):
+#
+#     print(f"{i:2} |", end="")
+#
+#
+#     for j in range(1, 11):
+#         print(f"{i * j:4}", end="")
+
+
