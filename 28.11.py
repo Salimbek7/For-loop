0e89e8b4e8eafb3e1d3688f32d824ef34e41c9c9
Qurbonboyev Salimbek <matniyozovasharofat9@gmail.com>
2025-12-01 15:02:40 +0300

    Add files via upload

---
+++ 28.11.py
@@ -0,0 +1,17 @@
+
+# a = int(input("Sonni kiriting: "))
+# yigindi = 0
+# b = a
+# while a > 0:
+#     raqam = b % 10
+#     yigindi = yigindi + raqam
+#     b = a // 10
+#
+# print(f"{a} Raqamlar yig'indisi: {yigindi}", )
+
+
+
+
+
+
+
